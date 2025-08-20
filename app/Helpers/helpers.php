1ac0dd44f9199842ef3a2e151868666c5cfe9ba6
ahmed abad <ahmed.abad@bootfi.com>
2025-08-20 10:15:02 +0300

    run composer pint

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,20 +2,17 @@
 
 declare(strict_types=1);
 
-use App\Exceptions\LogicalException;
 use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
-use Kreait\Laravel\Firebase\Facades\Firebase;
 
-if ( ! function_exists('isAuthenticatedAdmin')) {
+if (! function_exists('isAuthenticatedAdmin')) {
     function isAuthenticatedAdmin(): bool
     {
         return Auth::guard('admin-api')->check();
     }
 }
 
-if ( ! function_exists('getCurrentAdmin')) {
+if (! function_exists('getCurrentAdmin')) {
     function getCurrentAdmin(): Authenticatable
     {
         return Auth::guard('admin-api')->user();
